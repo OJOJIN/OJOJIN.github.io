44161d0a933f9ba0187a868ca2c638f4e8a99539
OJOJIN <ojy101039@gmail.com>
2023-07-06 03:22:20 +0900

    init blog

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "Jinyoung's Log",
+  description: "Hello :) I'm Oh Jinyoung",
+  author: "OJOJIN",
+  siteUrl: "https://ojojin.github.io/",
+  links: {
+    github: "https://github.com/OJOJIN",
+    // linkedIn: "https://linkedin.com",
+    // facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com/ojin_y/",
+    email: "mailto:ojy101039@gmail.com",
+  },
+  utterances: {
+    repo: "OJOJIN/OJOJIN.github.io",
+    type: "pathname",
+  },
+}
